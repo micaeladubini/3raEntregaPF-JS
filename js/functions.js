f41725581e32710d1bbde5b424c0c82f9d61065b
Micaela Dubini <88872273+micaeladubini@users.noreply.github.com>
2021-12-02 21:12:19 -0300

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -115,13 +115,10 @@ window.onload = function () {
 
     //Evento para añadir un producto al carrito de la compra
     function anyadirProductoAlCarrito(evento) {
-        // Anyadimos el Nodo a nuestro carrito
+        // Añadimos el Nodo a nuestro carrito
         carrito.push(evento.target.getAttribute('marcador'));
-        // Calculo el total
         calcularTotal();
-        // Actualizamos el carrito 
         renderizarCarrito();
-        // Actualizamos el LocalStorage
         guardarCarritoEnLocalStorage();
 
     }
@@ -130,25 +127,21 @@ window.onload = function () {
     function renderizarCarrito() {
         // Vaciamos todo el html
         DOMcarrito.textContent = '';
-        // Quitamos los duplicados
         const carritoSinDuplicados = [...new Set(carrito)];
-        // Generamos los Nodos a partir de carrito
         carritoSinDuplicados.forEach((item) => {
-            // Obtenemos el item que necesitamos de la variable base de datos
+  
             const miItem = baseDeDatos.filter((itemBaseDatos) => {
-                // ¿Coincide las id? Solo puede existir un caso
                 return itemBaseDatos.id === parseInt(item);
             });
             // Cuenta el número de veces que se repite el producto
             const numeroUnidadesItem = carrito.reduce((total, itemId) => {
-                // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
                 return itemId === item ? total += 1 : total;
             }, 0);
             // Creamos el nodo del item del carrito
             const miNodo = document.createElement('li');
             miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
             miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}U$`;
-            // Boton de borrar
+
             const miBoton = document.createElement('button');
             miBoton.classList.add('btn', 'btn-danger', 'mx-5');
             miBoton.textContent = 'X';
@@ -164,7 +157,7 @@ window.onload = function () {
                     showConfirmButton: false,
                 })
             });
-            // Mezclamos nodos
+
             miNodo.appendChild(miBoton);
             DOMcarrito.appendChild(miNodo);
         });
@@ -172,17 +165,14 @@ window.onload = function () {
 
     // Evento para borrar un elemento del carrito
     function borrarItemCarrito(evento) {
-        // Obtenemos el producto ID que hay en el boton pulsado
         const id = evento.target.dataset.item;
         // Borramos todos los productos
         carrito = carrito.filter((carritoId) => {
             return carritoId !== id;
         });
-        // volvemos a renderizar
+
         renderizarCarrito();
-        // Calculamos de nuevo el precio
         calcularTotal();
-        // Actualizamos el LocalStorage
         guardarCarritoEnLocalStorage();
     }
 
@@ -192,7 +182,6 @@ window.onload = function () {
         total = 0;
         // Recorremos el array del carrito
         carrito.forEach((item) => {
-            // De cada elemento obtenemos su precio
             const miItem = baseDeDatos.filter((itemBaseDatos) => {
                 return itemBaseDatos.id === parseInt(item);
             });
@@ -202,7 +191,7 @@ window.onload = function () {
         DOMtotal.textContent = total.toFixed(2);
     }
 
-    // Varia el carrito y vuelve a dibujarlo
+
     function vaciarCarrito() {
         // Limpiamos los productos guardados
         carrito = [];
@@ -210,7 +199,6 @@ window.onload = function () {
         // Renderizamos los cambios
         renderizarCarrito();
         calcularTotal();
-        // Borra LocalStorage
         localStorage.clear();
     }
 
